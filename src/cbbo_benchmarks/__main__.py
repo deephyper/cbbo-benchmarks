091346fcf7d29e9a428c08701d240882c21b95cf
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2025-03-31 13:44:39 -0400

    Add example

--- src/cbbo_benchmarks/__main__.py
+++ src/cbbo_benchmarks/__main__.py
@@ -1,10 +1,14 @@
 """Main entry point for package."""
 
+from .runner import Runner
 
 
 def main():
     """here."""
     print("Hello there!")
+
+    runner = Runner("cbbo.toml")
+    runner.run()
 
 
 if __name__ == "__main__":
